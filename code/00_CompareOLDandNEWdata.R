0c152ec7a6707a9cd22608ba7220a4e3bdb38120
jpark <jparkerpowell@gmail.com>
2024-11-11 08:25:43 +0100

    checked, no changes in data

--- code/00_CompareOLDandNEWdata.R
+++ code/00_CompareOLDandNEWdata.R
@@ -2,9 +2,9 @@ install.packages("dplyr")
 library("dplyr")   
 library("diffdf")
 
-data1 <- read.csv("data/HandelDiensten_raw.csv", sep = ",")
-data2 <- read.csv("data/HandelDiensten_raw1_2024_11_01.csv", sep = ",")
+data1 <- read.csv("data/cbs_basic_macro_NOT_SEASONCORRECTED_qt.csv", sep = ",")
+data2 <- read.csv("data/cbs_basic_macro_NOT_SEASONCORRECTED_qt_2024_11_11.csv", sep = ",")
 
-all_equal(data1, data2)  
+all.equal(data1, data2)  
 
 diffdf(data1, data2)
